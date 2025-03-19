f620735e628d7260b54eb11049998e72416e9002
Hylfrd <Hylfrd@outlook.com>
2025-03-19 11:56:10 +0800

    Merge remote changes

--- docs/.vitepress/utils.ts
+++ docs/.vitepress/utils.ts
@@ -5,11 +5,11 @@ export default {
         text: "HMCL崩溃指南讲解", 
         collapsed: false, 
         items: [
-          { text: "如何安装HMCL", link: "/hmcl-helper/guide/hmcl" }, 
-          { text: "如何安装Java", link: "/hmcl-helper/guide/java" },
-          { text: "如何打开自动安装界面", link: "/hmcl-helper/guide/install" },
-          { text: "如何打开配置文件夹", link: "/hmcl-helper/guide/config" },
-          { text: "如何找到latest.log", link: "/hmcl-helper/guide/latest" },
+          { text: "如何安装HMCL", link: "/guide/hmcl" }, // 移除 /hmcl-helper 前缀
+          { text: "如何安装Java", link: "/guide/java" },
+          { text: "如何打开自动安装界面", link: "/guide/install" },
+          { text: "如何打开配置文件夹", link: "/guide/config" },
+          { text: "如何找到latest.log", link: "/guide/latest" },
         ],
       },
     ];
