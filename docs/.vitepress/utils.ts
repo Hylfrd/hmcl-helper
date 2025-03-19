4228f673bc0c8a15d1698953a12baf05a40ad36d
Hylfrd <Hylfrd@outlook.com>
2025-03-19 15:35:13 +0800

    Guide Change

--- docs/.vitepress/utils.ts
+++ docs/.vitepress/utils.ts
@@ -14,6 +14,9 @@ export default {
           { text: "虚拟机参数问题", link: "/guide/jvm" },
           { text: "环境变量问题", link: "/guide/variables" },
           { text: "打开'查看日志'问题", link: "/guide/log" },
+          { text: "创建档案问题", link: "/guide/profile" },
+          { text: "启动器日志问题", link: "/guide/launcher" },
+          { text: "虚拟内存问题", link: "/guide/memory" },
         ],
       },
     ];
