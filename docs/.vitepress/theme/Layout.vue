31b52fb534461612e1446541c5858dea698d9536
Hylfrd <Hylfrd@outlook.com>
2026-06-27 00:48:09 +0800

    update theme

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="site-layout">
     <DefaultTheme.Layout />
-    <SiteFooter />
+    <SiteFooter v-if="isHome" />
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import SiteFooter from './SiteFooter.vue'
+
+const { frontmatter } = useData()
+
+const isHome = computed(() => frontmatter.value?.layout === 'home')
 </script>
